38a08adff5a930dc702e9464810718dcac7e7309
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2018-03-02 22:41:27 +0200

    Create project

---
+++ app/src/main/java/version/evening/canvas/flutrack/MainActivity.kt
@@ -0,0 +1,12 @@
+package version.evening.canvas.flutrack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
