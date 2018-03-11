619071deb09792341e8ecad15afecb716ecacc46
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2018-03-11 14:45:02 +0200

    Fix ktlint check

--- app/src/main/kotlin/version/evening/canvas/flutrack/SchedulersWrapper.kt
+++ app/src/main/kotlin/version/evening/canvas/flutrack/SchedulersWrapper.kt
@@ -5,6 +5,6 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class SchedulersWrapper {
-    fun android() : Scheduler = AndroidSchedulers.mainThread()
-    fun io() : Scheduler = Schedulers.io()
+    fun android(): Scheduler = AndroidSchedulers.mainThread()
+    fun io(): Scheduler = Schedulers.io()
 }
