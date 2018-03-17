746952607065f09228dbdda110a462c6331e41f3
Alexander Bondarenco <alexandru.bondarenco@travelportdigital.com>
2018-03-17 18:40:48 +0200

    Use support map fragment instead of map view

--- app/src/main/kotlin/version/evening/canvas/flutrack/map/MapFragment.kt
+++ app/src/main/kotlin/version/evening/canvas/flutrack/map/MapFragment.kt
@@ -1,25 +1,20 @@
 package version.evening.canvas.flutrack.map
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.fragment_map.*
-import kotlinx.android.synthetic.main.fragment_map.view.*
 import version.evening.canvas.flutrack.FlutrackApplication
 import version.evening.canvas.flutrack.R
 import javax.inject.Inject
 
-class MapFragment : Fragment() {
-    private lateinit var mapView: MapView
-
+class MapFragment : SupportMapFragment() {
     @Inject
     lateinit var viewModel: MapViewModel
 
@@ -39,24 +34,22 @@ class MapFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = layoutInflater.inflate(R.layout.fragment_map, container, false)
+        val view = super.onCreateView(inflater, container, savedInstanceState) ?:
+                throw IllegalStateException("google maps didn't create view")
 
         val infoWindowAdapter = TweetInfoWindowAdapter(context!!)
 
-        this.mapView = view.mapView.apply {
-            onCreate(savedInstanceState)
-            getMapAsync { map ->
-                viewModel.tweetsObservable.subscribe {
-                    val markerOptions = MarkerOptions().apply {
-                        position(it.latLng)
-                    }
-                    val marker = map.addMarker(markerOptions)
-                    infoWindowAdapter.registerMarkerData(marker, it)
+        getMapAsync { map ->
+            viewModel.tweetsObservable.subscribe {
+                val markerOptions = MarkerOptions().apply {
+                    position(it.latLng)
                 }
-                map.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style))
-                map.setInfoWindowAdapter(infoWindowAdapter)
-                map.uiSettings.isMapToolbarEnabled = false
+                val marker = map.addMarker(markerOptions)
+                infoWindowAdapter.registerMarkerData(marker, it)
             }
+            map.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style))
+            map.setInfoWindowAdapter(infoWindowAdapter)
+            map.uiSettings.isMapToolbarEnabled = false
         }
 
         return view
@@ -67,50 +60,11 @@ class MapFragment : Fragment() {
         disposables.add(viewModel.errorObservable.subscribe {
             dataErrorSubject.onNext(Unit)
         })
-        disposables.add(viewModel.firstObservable.subscribe {
-            mapView.visibility = View.VISIBLE
-            mapProgressBar.visibility = View.GONE
-        })
     }
 
     override fun onDestroyView() {
         disposables.clear()
         super.onDestroyView()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
     }
 }
 
